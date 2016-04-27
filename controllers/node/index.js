4f456efe6b9b62291a3fae8726e12012d8026701
Ross Chater <rcapple@me.com>
2016-04-27 22:46:40 +0100

    Added docker-remote-api

--- controllers/node/index.js
+++ controllers/node/index.js
@@ -4,56 +4,77 @@
 
 var Node = require('../../models/node');
 
-exports.before = function(req, res, next){
-  var id = req.params.node_id;
-  if (!id) return next();
-  // pretend to query a database...
-  process.nextTick(function(){
-      Node.findOne({ _id: id }, function(err, node) {
-          req.node = node;
-          // cant find that host
-          if (!req.node) return next('route');
-          // found it, move on to the routes
-          next();
-      });
-  });
-};
-
-exports.list = function(req, res, next){
-    Node.find({}, function(err, nodes) {
-        res.render('list', { nodes: nodes });
+exports.before = function(req, res, next) {
+    var id = req.params.node_id;
+    if (!id) return next();
+    // pretend to query a database...
+    process.nextTick(function() {
+        Node.findOne({
+            _id: id
+        }, function(err, node) {
+            req.node = node;
+            // cant find that host
+            if (!req.node) return next('route');
+            // found it, move on to the routes
+            next();
+        });
     });
 };
 
-exports.edit = function(req, res, next){
-  res.render('edit', { node: req.node });
+exports.list = function(req, res, next) {
+    Node.find({}, function(err, nodes) {
+        res.render('list', {
+            nodes: nodes
+        });
+    });
 };
 
-exports.add = function(req, res, next){
-  res.render('add');
+exports.edit = function(req, res, next) {
+    res.render('edit', {
+        node: req.node
+    });
 };
 
-exports.create = function(req, res, next){
-  var body = req.body;
-  var node = new Node({
-      name: body.node.name,
-      ip: body.node.ip
-  });
-  console.log(node);
-  node.save(function(err){
-      if (err) throw err;
-      res.message('Information updated!');
-      res.redirect('/node/' + node._id);
-  });
+exports.add = function(req, res, next) {
+    res.render('add');
 };
 
-exports.show = function(req, res, next){
-  res.render('show', { node: req.node });
+exports.create = function(req, res, next) {
+    var body = req.body;
+    var node = new Node({
+        name: body.node.name,
+        ip: body.node.ip
+    });
+    console.log(node);
+    node.save(function(err) {
+        if (err) throw err;
+        res.message('Information updated!');
+        res.redirect('/node/' + node._id);
+    });
 };
 
-exports.update = function(req, res, next){
-  var body = req.body;
-  req.node.name = body.node.name;
-  res.message('Information updated!');
-  res.redirect('/host/' + req.node.id);
+exports.show = function(req, res, next) {
+
+    var docker = require('docker-remote-api');
+    var request = docker({
+        host: '/var/run/docker.sock'
+    });
+
+    request.get('/containers/json', {
+        json: true
+    }, function(err, containers) {
+        if (err) throw err
+        console.log('containers', containers)
+        // res.render('show', {
+        //     node: req.node
+        // });
+    });
+
+};
+
+exports.update = function(req, res, next) {
+    var body = req.body;
+    req.node.name = body.node.name;
+    res.message('Information updated!');
+    res.redirect('/host/' + req.node.id);
 };
--- models/node.js
+++ models/node.js
@@ -1,40 +1,15 @@
 // grab the things we need
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
-var ObjectId   = mongoose.ObjectId;
-
-// create a schema
-var containerSchema = new Schema({
-  name: String,
-  image: String,
-  created: String,
-});
-
-// create a schema
-var volumeSchema = new Schema({
-  location: String,
-  description: String,
-  activeContainers: String,
-});
-
-// create a schema
-var portSchema = new Schema({
-  number: String,
-});
 
 // create a schema
 var nodeSchema = new Schema({
   name: String,
   ip: String,
   created_at: Date,
-  updated_at: Date,
-  containers: [containerSchema],
-  volumes: [volumeSchema],
-  ports: [portSchema],
+  updated_at: Date
 });
 
-// the schema is useless so far
-// we need to create a model using it
 var Node = mongoose.model('Node', nodeSchema);
 
 // make this available to our users in our Node applications
